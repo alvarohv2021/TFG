edbe1f3ad327b1819a051c1cf7bda4c4002046ac
alvarohv2021 <alvarohv2013@gmail.com>
2022-05-26 19:34:42 +0200

    añadida la insercion de usuarios en la base de datos

--- back/Modelo/m_usuario.php
+++ back/Modelo/m_usuario.php
@@ -25,4 +25,19 @@ function comprobarUsuario($nombre, $password)
     }
 }
 
+function insertarUsuarios($nombre, $email, $password)
+{
+    global $coon;
+
+    $usuario=comprobarUsuario($nombre, $password);
+
+    if ($usuario->getId()==0) {
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $sql = "insert into Usuarios(Email,Username,Pasword) values ('" . $email . "','" . $nombre . "','" . $password . "')";
+        $coon->query($sql);
+        return true;
+    } else {
+        return comprobarUsuario($nombre, $password);
+    }
+}
 ?>
