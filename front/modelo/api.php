f7949157dd0af789c5f124fc13d9e490c452c050
alvarohv2021 <alvarohv2013@gmail.com>
2022-05-28 18:33:36 +0200

    unificado

--- front/modelo/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$api="http://localhost:8055/items";
-
-$provincias=json_decode(file_get_contents($api."/Provincias"), true)["data"];
-$casas=json_decode(file_get_contents($api."/Casas"), true)["data"];
-
-?>
