b08696a269196595c739d7ae17d0d738467366f0
alvarohv2021 <alvarohv2013@gmail.com>
2022-06-01 22:38:46 +0200

    Añadidos estilos para paginas de registro e inicio de sesion

--- vista/inicio.php
+++ vista/inicio.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8">
     <title>Inicio de sesion</title>
     <link href="../estilos/styles.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/botones.css" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 </head>
 
@@ -13,32 +14,30 @@
         <form method="post" action="../Controladores/c_inicio.php">
             <div class="form-group row">
                 <div class="col-3 text-center mt-4">
-                    <h5><a class="negro" href="../Controladores/c_provincias.php" style="text-decoration: none">Spain
+                    <h5><a href="../Controladores/c_provincias.php" style="text-decoration: none">Spain
                             Travels</a></h5>
                 </div>
-                <div class="col-9 negro offset-4">
+                <div class="col-12 negro text-center">
                     <h2>Inicio de sesion</h2>
                 </div>
                 <label class="sr-only" for="name">User Name</label>
                 <div class="col-12">
-                    <input type="text" class="form-control mb-2 mr-2 text-center" name="name" id="uName"
-                        placeholder="User Name">
+                    <input type="text" class="form-control mb-2 mr-2 text-center" name="name" id="uName" placeholder="User Name">
                 </div>
                 <label class="sr-only" for="password">password</label>
                 <div class="col-12">
-                    <input type="password" class="form-control mb-2 mr-2 text-center" name="password" id="password"
-                        placeholder="Password">
+                    <input type="password" class="form-control mb-2 mr-2 text-center" name="password" id="password" placeholder="Password">
                 </div>
             </div>
 
 
             <div class="col-6 float-left">
-                <button type="submit" class="btn btn-success bverde w-100 mb-2">Sign In</button>
+                <button type="submit" class="btn-inicio w-100 mb-2">Sign In</button>
             </div>
         </form>
         <div class="col-6 offset-6">
             <a href="../Vista/registro.php">
-                <button class="btn btn-primary bazul w-100 mb-2">Register</button>
+                <button class="btn-registro w-100 mb-2">Register</button>
             </a>
         </div>
     </div>
--- vista/registro.php
+++ vista/registro.php
@@ -4,19 +4,21 @@
 <head>
     <meta charset="UTF-8">
     <title>Registro</title>
+    <link href="../estilos/styles.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/botones.css" type="text/css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
 </head>
 
 <body>
-    <div class="container bg-danger rounded mt-5">
+    <div class="container bdorado rounded mt-5">
         <form method="post" action="../Controladores/c_registro.php">
 
             <div class="form-group row">
                 <div class="col-3 text-center mt-4">
-                    <h5><a class="text-light" href="../Controladores/c_provincias.php" style="text-decoration: none">Spain
+                    <h5><a href="../Controladores/c_provincias.php" style="text-decoration: none">Spain
                             Travels</a></h5>
                 </div>
-                <div class="col-9 text-light offset-4">
+                <div class="col-12 negro text-center">
                     <h2>Inicio de sesion</h2>
                 </div>
                 <label class="sr-only" for="name">User Name</label>
@@ -38,12 +40,12 @@
             </div>
 
             <div class="col-6 float-left">
-                <button type="submit" class="btn btn-primary w-100 mb-2">Register</button>
+                <button type="submit" class="btn-registro w-100 mb-2">Register</button>
             </div>
         </form>
         <div class="col-6 offset-6">
             <a href="../Vista/inicio.php">
-            <button class="btn btn-success w-100 mb-2">Sign In</button>
+                <button class="btn-inicio w-100 mb-2">Sign In</button>
             </a>
         </div>
 
