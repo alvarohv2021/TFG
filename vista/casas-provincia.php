842c49dc8aced37b2a2c9b9969c8c6a211ee4162
alvarohv2021 <alvarohv2013@gmail.com>
2022-06-06 00:33:36 +0200

    maquetand, dando estilos y arreglando el responsivenes

--- vista/casas-provincia.php
+++ vista/casas-provincia.php
@@ -1,4 +1,20 @@
-<?php include_once("header.html") ?>
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Spain Homes</title>
+
+    <link href="../imagenes/icono/hause.png" type="text/css" rel="icon">
+
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <script src="https://kit.fontawesome.com/d08437ea27.js" crossorigin="anonymous"></script>
+    <link href="../estilos/styles.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/e_colores.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/e_casas.css" type="text/css" rel="stylesheet">
+
+</head>
 
 <body>
     <?php include_once("barraSuperior.php") ?>
@@ -15,10 +31,10 @@
         for ($i = 0; $i < count($objCasasProvincia); $i++) {
         ?>
             <div class='row mb-4 bdorado ofertaCasa'>
-                <div class='col-md-3 col-12 p-0'>
+                <div class='col-lg-3 col-12 p-0'>
                     <img src="<?php echo $objCasasProvincia[$i]->getRutaImagen() ?>" alt="">
                 </div>
-                <div class='col-md-6 col-12'>
+                <div class='col-lg-6 col-sm-12 col-12'>
                     <div class='row'>
                         <div class='col-12 pt-2'>
                             <a href='../Controladores/c_casa.php?idCasa=<?php echo $objCasasProvincia[$i]->getId() ?>&publicacion=<?php $propietario = $objCasasProvincia[$i]->getPropietario(); if ($propietario == $usuario->id){ echo true;} ?>' target="_self">
@@ -30,7 +46,7 @@
                         </div>
                     </div>
                 </div>
-                <div class='col-md-3 col-12 bazul'>
+                <div class='col-lg-3 col-12 bazul'>
                     <div class='row'>
                         <div class='col-12'>
                             <h4><?php echo $objCasasProvincia[$i]->getTipo() ?>/<?php echo $objCasasProvincia[$i]->getOferta() ?></h4>
--- vista/perfil.php
+++ vista/perfil.php
@@ -1,5 +1,21 @@
-<?php include_once("header.html") ?>
+<!DOCTYPE html>
+<html lang="es">
 
+<head>
+    <meta charset="UTF-8">
+    <title>Spain Homes</title>
+
+    <link href="../imagenes/icono/hause.png" type="text/css" rel="icon">
+
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <script src="https://kit.fontawesome.com/d08437ea27.js" crossorigin="anonymous"></script>
+    <link href="../estilos/styles.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/e_colores.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/e_casas.css" type="text/css" rel="stylesheet">
+    <link href="../estilos/e_perfil.css" type="text/css" rel="stylesheet">
+
+</head>
 <body>
     <?php include_once("barraSuperior.php") ?>
     <!--**************************Contenido Principal**************************-->
